a8c3d7433b46617efd8d94904fcf7f355e3bd8f8
elianmtg <elianmtoribio@gmail.com>
2021-08-27 20:50:50 -0400

    conection db

--- RitmoApi/RitmoApi/Startup.cs
+++ RitmoApi/RitmoApi/Startup.cs
@@ -2,10 +2,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RitmoApi.Context;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +28,8 @@ namespace RitmoApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Conexion")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
