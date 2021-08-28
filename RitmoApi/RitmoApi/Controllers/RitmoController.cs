2ccf811e98123b7fc0e35f7a34dab01c861a21dc
elianmtg <elianmtoribio@gmail.com>
2021-08-27 21:17:46 -0400

    CRUD api ready

--- RitmoApi/RitmoApi/Controllers/RitmoController.cs
+++ RitmoApi/RitmoApi/Controllers/RitmoController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RitmoApi.Context;
+using RitmoApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,36 +15,111 @@ namespace RitmoApi.Controllers
     [ApiController]
     public class RitmoController : ControllerBase
     {
+        private readonly AppDbContext context;
+
+        public RitmoController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
         // GET: api/<RitmoController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public ActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+            try
+            {
+                return Ok(context.CANTANTES.ToList());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            //try
+            //{
+
+            //}
+            //catch (Exception ex)
+            //{
+
+            //}
         }
 
         // GET api/<RitmoController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        [HttpGet("{id}", Name ="GetRitmo")]
+        public ActionResult Get(int id)
         {
-            return "value";
+            try
+            {
+                var cantantes = context.CANTANTES.FirstOrDefault(c => c.ID == id);
+                return Ok(cantantes);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         // POST api/<RitmoController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public ActionResult Post([FromBody]Ritmo ritmo)
         {
+            try
+            {
+                context.CANTANTES.Add(ritmo);
+                context.SaveChanges();
+                return CreatedAtRoute("GetRitmo", new { id = ritmo.ID }, ritmo);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         // PUT api/<RitmoController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public ActionResult Put(int id, [FromBody]Ritmo ritmo)
         {
+            try
+            {
+                if (ritmo.ID == id)
+                {
+                    context.Entry(ritmo).State = EntityState.Modified;
+                    context.SaveChanges();
+                    return CreatedAtRoute("GetRitmo", new { id = ritmo.ID }, ritmo);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         // DELETE api/<RitmoController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public ActionResult Delete(int id)
         {
+            try
+            {
+                var ritmo = context.CANTANTES.FirstOrDefault(c => c.ID == id);
+                if (ritmo != null)
+                {
+                    context.CANTANTES.Remove(ritmo);
+                    context.SaveChanges();
+                    return Ok(id);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
