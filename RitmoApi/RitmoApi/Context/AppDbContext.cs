cf240259e46065e8c9bb68dd9b409598832251fb
elianmtg <elianmtoribio@gmail.com>
2021-08-27 20:28:15 -0400

    adding context

---
+++ RitmoApi/RitmoApi/Context/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RitmoApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RitmoApi.Context
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+
+        }
+        public DbSet<Ritmo> CANTANTES { get; set; }
+    }
+}
---
+++ RitmoApi/RitmoApi/Models/Ritmo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RitmoApi.Models
+{
+    public class Ritmo
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string NOMBRE { get; set; }
+        public string APELLIDO { get; set; }
+        public string ALIAS { get; set; }
+        public string GENERO { get; set; }
+        public string NACIONALIDAD { get; set; }
+    }
+}
